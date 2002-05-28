e8c5ca3617114fa3833f59153cdab9cdb7bc4470
Roland McGrath <roland@gnu.org>
2002-05-28 07:34:54 +0000

    2002-05-28  Roland McGrath  <roland@frob.com>
    * alpha/alpha/alphaasm.sym: New file.
    	* alpha/alpha/task.h: New file.
    	* alpha/alpha/context.h (jmp_buf): Define as `struct jmp_buf' too.
    	* alpha/alpha/prom_interface.h: Fix obsolescent #else/#endif syntax.
    	* alpha/alpha/pmap.c: Likewise.
    	* alpha/alpha/pmap.h: Likewise.
    	* alpha/alpha/pcb.c: Likewise.
    	* alpha/alpha/context.h: Likewise.
    	* alpha/alpha/thread.h: Likewise.
    	* alpha/include/mach/alpha/alpha_instruction.h: Likewise.
    	* alpha/include/mach/alpha/vm_param.h: Likewise.
    	* alpha/include/mach/alpha/vm_types.h: Likewise.
    	* alpha/include/mach/alpha/boolean.h: Likewise.
    	* alpha/include/mach/alpha/kern_return.h: Likewise.
    	* alpha/include/mach/alpha/thread_status.h: Likewise.
    	* alpha/include/mach/alpha/syscall_sw.h: Likewise.
    	* alpha/include/mach/alpha/exception.h: Likewise.
    	* alpha/alpha/alpha_copyin.S: Likewise.
    	* alpha/alpha/machspl.h: Likewise.
    	* alpha/alpha/trap.c: Likewise.
    	* alpha/alpha/alpha_instruction.c: Likewise.
    	* alpha/alpha/alpha_init.c: Likewise.
    	* alpha/alpha/setjmp.h: Likewise.
    	* alpha/alpha/vm_tuning.h: Likewise.

--- alpha/alpha/context.h
+++ alpha/alpha/context.h
@@ -1,38 +1,41 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1992 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
 /*
  * HISTORY
- * $Log:	context.h,v $
+ * $Log: context.h,v $
+ * Revision 1.1  2002/05/28 06:27:03  roland
+ * Alpha support files verbatim from CMU release MK83a.
+ *
  * Revision 2.2  93/01/14  17:12:20  danner
  * 	Added reference to documentation source(s).
  * 	[92/12/16  15:12:17  af]
- * 
+ *
  * 	Created.
  * 	[92/05/31            af]
- * 
+ *
  *	Created.
  *
  */
@@ -54,7 +57,7 @@
 #define	_ALPHA_CONTEXT_H_	1
 
 #ifndef	ASSEMBLER
-typedef struct {
+typedef struct jmp_buf {
 	long	s0;
 	long	s1;
 	long	s2;
@@ -81,5 +84,5 @@ typedef struct hw_pcb {
 	long		pal_scratch[6];
 } *hw_pcb_t;
 
-#endif	ASSEMBLER
-#endif	_ALPHA_CONTEXT_H_
+#endif	/* ASSEMBLER */
+#endif	/* _ALPHA_CONTEXT_H_ */
