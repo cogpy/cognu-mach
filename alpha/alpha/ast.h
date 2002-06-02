f3cd7b03f1557751391b23a89c58a50757081011
Roland McGrath <roland@gnu.org>
2002-06-02 22:22:49 +0000

    2002-06-02  Roland McGrath  <roland@frob.com>
    * alpha/alpha/ast.h: Fix obsolescent #endif syntax.

--- alpha/alpha/ast.h
+++ alpha/alpha/ast.h
@@ -1,35 +1,38 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1992 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
 /*
  * HISTORY
- * $Log:	ast.h,v $
+ * $Log: ast.h,v $
+ * Revision 1.1  2002/05/28 06:27:03  roland
+ * Alpha support files verbatim from CMU release MK83a.
+ *
  * Revision 2.2  93/01/14  17:11:43  danner
  * 	Created.
  * 	[92/05/31            af]
- * 
+ *
  */
 /*
  *	File: ast.h
@@ -42,4 +45,4 @@
 #ifndef	_ALPHA_AST_H_
 #define	_ALPHA_AST_H_
 
-#endif	_ALPHA_AST_H_
+#endif	/* _ALPHA_AST_H_ */
