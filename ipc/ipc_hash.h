3772bda3f361c380925da968fd7a97521b18fc60
Roland McGrath <roland@gnu.org>
2002-06-02 22:22:41 +0000

    2002-06-01  Roland McGrath  <roland@frob.com>
    * ipc/ipc_hash.h (ipc_hash_local_delete): Uncomment prototype decls.

--- ipc/ipc_hash.h
+++ ipc/ipc_hash.h
@@ -44,22 +44,18 @@ ipc_hash_init();
 
 #if	MACH_IPC_DEBUG
 
-extern unsigned int
-ipc_hash_info(/* hash_info_bucket_t *, unsigned int */);
+extern unsigned int ipc_hash_info(hash_info_bucket_t *, unsigned int);
 
 #endif	/* MACH_IPC_DEBUG */
 
-extern boolean_t
-ipc_hash_lookup(/* ipc_space_t space, ipc_object_t obj,
-		   mach_port_t *namep, ipc_entry_t *entryp */);
+extern boolean_t ipc_hash_lookup(ipc_space_t space, ipc_object_t obj,
+				 mach_port_t *namep, ipc_entry_t *entryp);
 
-extern void
-ipc_hash_insert(/* ipc_space_t space, ipc_object_t obj,
-		   mach_port_t name, ipc_entry_t entry */);
+extern void ipc_hash_insert(ipc_space_t space, ipc_object_t obj,
+			    mach_port_t name, ipc_entry_t entry);
 
-extern void
-ipc_hash_delete(/* ipc_space_t space, ipc_object_t obj,
-		   mach_port_t name, ipc_entry_t entry */);
+extern void ipc_hash_delete(ipc_space_t space, ipc_object_t obj,
+			    mach_port_t name, ipc_entry_t entry);
 
 /*
  *	For use by functions that know what they're doing:
@@ -67,28 +63,24 @@ ipc_hash_delete(/* ipc_space_t space, ipc_object_t obj,
  *	and the local primitives, for table entries.
  */
 
-extern boolean_t
-ipc_hash_global_lookup(/* ipc_space_t space, ipc_object_t obj,
-			  mach_port_t *namep, ipc_tree_entry_t *entryp */);
+extern boolean_t ipc_hash_global_lookup(ipc_space_t space, ipc_object_t obj,
+					mach_port_t *namep,
+					ipc_tree_entry_t *entryp);
 
-extern void
-ipc_hash_global_insert(/* ipc_space_t space, ipc_object_t obj,
-			  mach_port_t name, ipc_tree_entry_t entry */);
+extern void ipc_hash_global_insert(ipc_space_t space, ipc_object_t obj,
+				   mach_port_t name, ipc_tree_entry_t entry);
 
-extern void
-ipc_hash_global_delete(/* ipc_space_t space, ipc_object_t obj,
-			  mach_port_t name, ipc_tree_entry_t entry */);
+extern void ipc_hash_global_delete(ipc_space_t space, ipc_object_t obj,
+				   mach_port_t name, ipc_tree_entry_t entry);
 
-extern boolean_t
-ipc_hash_local_lookup(/* ipc_space_t space, ipc_object_t obj,
-			 mach_port_t *namep, ipc_entry_t *entryp */);
+extern boolean_t ipc_hash_local_lookup(ipc_space_t space, ipc_object_t obj,
+				       mach_port_t *namep,
+				       ipc_entry_t *entryp);
 
-extern void
-ipc_hash_local_insert(/* ipc_space_t space, ipc_object_t obj,
-			 mach_port_index_t index, ipc_entry_t entry */);
+extern void ipc_hash_local_insert(ipc_space_t space, ipc_object_t obj,
+				  mach_port_index_t index, ipc_entry_t entry);
 
-extern void
-ipc_hash_local_delete(/* ipc_space_t space, ipc_object_t obj,
-			 mach_port_index_t index, ipc_entry_t entry */);
+extern void ipc_hash_local_delete(ipc_space_t space, ipc_object_t obj,
+				  mach_port_index_t index, ipc_entry_t entry);
 
 #endif	/* _IPC_IPC_HASH_H_ */
