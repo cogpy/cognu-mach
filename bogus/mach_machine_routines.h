aee8dbbe33adbe1a55a9a811c37c4e159a22832c
Roland McGrath <roland@gnu.org>
2002-06-24 00:34:35 +0000

    2002-06-23  Roland McGrath  <roland@frob.com>
    * i386/i386/mach_machine_routines.h: New file.
    	* bogus/mach_machine_routines.h (MACH_MACHINE_ROUTINES): Define to 0.

--- bogus/mach_machine_routines.h
+++ bogus/mach_machine_routines.h
@@ -1,1 +1,4 @@
-#define MACH_MACHINE_ROUTINES 1
+/* This file is overridden by a CPU-specific file if it wants to set
+   MACH_MACHINE_ROUTINES to 1.  */
+
+#define MACH_MACHINE_ROUTINES 0
---
+++ i386/i386/mach_machine_routines.h
@@ -0,0 +1,2 @@
+/* This file overrides bogus/mach_machine_routines.h to define this.  */
+#define MACH_MACHINE_ROUTINES 1
