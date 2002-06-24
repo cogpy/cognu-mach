e7dd1cef1ddadd01df093bf0ebbccc2515ed7c2d
Roland McGrath <roland@gnu.org>
2002-06-24 00:34:48 +0000

    2002-06-23  Roland McGrath  <roland@frob.com>
    * alpha/alpha/cpu_number.h (cpu_number): Declare only if [NCPUS > 1].
    	[! ASSEMBLER]: Include kern/cpu_number.h like i386 file does.

--- alpha/alpha/cpu_number.h
+++ alpha/alpha/cpu_number.h
@@ -1,35 +1,38 @@
-/* 
+/*
  * Mach Operating System
  * Copyright (c) 1992 Carnegie Mellon University
  * All Rights Reserved.
- * 
+ *
  * Permission to use, copy, modify and distribute this software and its
  * documentation is hereby granted, provided that both the copyright
  * notice and this permission notice appear in all copies of the
  * software, derivative works or modified versions, and any portions
  * thereof, and that both notices appear in supporting documentation.
- * 
+ *
  * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
  * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
  * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
- * 
+ *
  * Carnegie Mellon requests users of this software to return to
- * 
+ *
  *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
  *  School of Computer Science
  *  Carnegie Mellon University
  *  Pittsburgh PA 15213-3890
- * 
+ *
  * any improvements or extensions that they make and grant Carnegie Mellon
  * the rights to redistribute these changes.
  */
 /*
  * HISTORY
- * $Log:	cpu_number.h,v $
+ * $Log: cpu_number.h,v $
+ * Revision 1.1  2002/05/28 06:27:03  roland
+ * Alpha support files verbatim from CMU release MK83a.
+ *
  * Revision 2.2  93/01/14  17:12:30  danner
  * 	Created.
  * 	[92/12/10  14:55:43  af]
- * 
+ *
  */
 /*
  *	File: cpu_number.h
@@ -44,6 +47,16 @@
 #ifndef	_ALPHA_CPU_NUMBER_H_
 #define	_ALPHA_CPU_NUMBER_H_
 
+#include <cpus.h>
+
+#if	NCPUS > 1
+
 extern unsigned long	cpu_number();
+
+#endif
+
+#ifndef ASSEMBLER
+#include "kern/cpu_number.h"
+#endif
 
 #endif	/* _ALPHA_CPU_NUMBER_H_ */
