e2090c2e64854892809481bda8970cbeedd9ce80
Roland McGrath <roland@gnu.org>
2003-03-06 03:59:16 +0000

    removed

--- acconfig.h
+++
@@ -1,7 +0,0 @@
-/* Define this to the maximum number of CPUs to support,
-   i.e. 1 for uniprocessor or 2 or more for SMP.  */
-#undef NCPUS
-
-/* Define this to nonzero to enable the software FPU emulator,
-   used on machines that have no FPU.  */
-#undef FPE
