3fe9ffe752acac1e14b61f41fd3d32cf7751854f
Thomas Schwinge <thomas@schwinge.name>
2011-10-10 12:26:09 +0200

    * linux/dev/arch/i386/kernel/irq.c (install_user_intr_handler): First parameter is named irq.

--- linux/dev/arch/i386/kernel/irq.c
+++ linux/dev/arch/i386/kernel/irq.c
@@ -284,7 +284,7 @@ setup_x86_irq (int irq, struct linux_action *new)
 }
 
 int
-install_user_intr_handler (unsigned int line, unsigned long flags,
+install_user_intr_handler (unsigned int irq, unsigned long flags,
 			  ipc_port_t dest)
 {
   struct linux_action *action;
