3f81a1163c40c12ca1dcf75c93b76ba8e81054bd
Etienne Brateau <etienne.brateau@gmail.com>
2020-03-19 20:46:31 +0100

    Update xen.c for x64

--- x86_64/xen.c
+++ x86_64/xen.c
@@ -46,18 +46,19 @@ void hyp_failsafe_c_callback(struct failsafe_callback_regs *regs) {
 
 extern void return_to_iret;
 
-void hypclock_machine_intr(int old_ipl, void *ret_addr, struct i386_interrupt_state *regs, unsigned64_t delta) {
+void hypclock_machine_intr(int old_ipl, void *ret_addr, struct i386_interrupt_state *regs, uint64_t delta) {
 	if (ret_addr == &return_to_iret) {
 		clock_interrupt(delta/1000,		/* usec per tick */
 			(regs->efl & EFL_VM) ||		/* user mode */ 
 			((regs->cs & 0x02) != 0),	/* user mode */ 
-			old_ipl == SPL0);		/* base priority */
+			old_ipl == SPL0,		/* base priority */
+			regs->eip);			/* interrupted eip */
 	} else
-		clock_interrupt(delta/1000, FALSE, FALSE);
+		clock_interrupt(delta/1000, FALSE, FALSE, 0);
 }
 
 void hyp_p2m_init(void) {
-	unsigned long nb_pfns = atop(phys_last_addr);
+	unsigned long nb_pfns = vm_page_table_size();
 #ifdef MACH_PSEUDO_PHYS
 #define P2M_PAGE_ENTRIES (PAGE_SIZE / sizeof(unsigned long))
 	unsigned long *l3 = (unsigned long *)phystokv(pmap_grab_page()), *l2 = NULL;
