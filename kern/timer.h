3ff7d70ebe73103ccced9df33ceb6d449b9eef7e
drzo <d@rzo.io>
2025-08-12 22:37:40 +0200

    Update timer.h

--- kern/timer.h
+++ kern/timer.h
@@ -37,6 +37,11 @@
  */
 
 /*
+*   MICROSECONDS_PER_SECOND is referenced in kern/timer.c but is not defined anywhere.
+*/
+#define MICROSECONDS_PER_SECOND 1000000UL
+
+/*
  *	TIMER_MAX is needed if a 32-bit rollover timer needs to be adjusted for
  *	maximum value.
  */
